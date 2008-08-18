95024df3fa609a04a68746e580a5dbc9b79bb219
knutbo <knutbo@3f9bf807-9651-0410-a908-2dd406608930>
2008-08-18 08:09:16 +0000

    Webscrape mot bokkilden - under arbeid.

---
+++ trunk/BokServer/classes/webscrape/bokkilden.php
@@ -0,0 +1,87 @@
+<?php
+class Bokkilden {
+	function getBookInfo($isbn) {
+		return $this->getBookData($isbn);
+	}
+
+	function getBookData($isbn) {
+		$result = array ();
+
+		$cmd = AppConfig :: WGET . " -O- 'http://www.bokkilden.no/SamboWeb/avansertSok.do?isbn=$isbn&mp=rom'";
+		$klienturl = "http://www.bokkilden.no/SamboWeb/avansertSok.do?isbn=$isbn&mp=rom";
+		$result["url"] = $klienturl;
+
+		$data = array ();
+		$res = exec($cmd, & $data);
+
+		$regs = array ();
+		$allRaw = utf8_decode(implode($data, '###'));
+		preg_match("/class=\"bokfaktatabell\">(.*?)<\/table>/", $allRaw, & $regs);
+
+		$info = $this->splitIntoFields($regs[1]);
+		$result["info"] = $info;
+		return $result;
+	}
+
+	function splitIntoFields($one) {
+		$one = str_replace(array (
+			"<tr>",
+			"</tr>",
+			"<b>",
+			"</b>",
+			"</br>"
+		), "", $one);
+
+		$matches = array ();
+		preg_match_all("/<td.*?>(.*?)<\/td>/", $one, & $matches);
+
+		$lookup = array (
+			"Utgitt:" => "published_year",
+			"Utgave:" => "edition",
+			"ISBN:" => "isbn",
+			"Alder:" => "note",
+			"Genre:" => "genre",
+			"Innb.:" => "note",
+			"Sider:" => "note",
+			"Forlag:" => "publisher"
+		);
+
+		$result = array ();
+
+		$k = null;
+		$orgKey = null;
+		foreach ($matches[1] as $field) {
+			$field = trim($field);
+
+			$orgtittel = array ();
+			if (preg_match("/Originaltittel:(.*)/", $field, & $orgtittel)) {
+				$result["org_title"] = trim($orgtittel[1]);
+				continue;
+			}
+
+			if (preg_match("/Spr.*:/", $field, & $orgtittel)) {
+				$k = "note";
+				$orgKey = "Spr&aring;k:";
+				continue;
+			} else if (array_key_exists($field, $lookup)) {
+				$k = $lookup[$field];
+				$orgKey = $field;
+				continue;
+			}
+			
+			if ($k == "note") {
+				$field = "$orgKey $field";
+			}
+
+			if (array_key_exists($k, $result)) {
+				$result[$k] .= "<br>" . $field;
+			} else {
+				$result[$k] = $field;
+			}
+		}
+
+		return $result;
+	}
+
+}
+?>
--- trunk/BokServer/services/webscrape/webscrape.php
+++ trunk/BokServer/services/webscrape/webscrape.php
@@ -4,12 +4,13 @@ include_once ("../../classes/auth/User.php");
 include_once ("../../classes/util/DB.php");
 include_once ("../../classes/auth/RegnSession.php");
 include_once ("../../classes/webscrape/bibsys.php");
+include_once ("../../classes/webscrape/bokkilden.php");
 
-$action = array_key_exists("action", $_REQUEST) ? $_REQUEST["action"] : "bibsys";
-$isbn = array_key_exists("isbn", $_REQUEST) ? $_REQUEST["isbn"] : "8270342327";
+$action = array_key_exists("action", $_REQUEST) ? $_REQUEST["action"] : "bokkilden";
+$isbn = array_key_exists("isbn", $_REQUEST) ? $_REQUEST["isbn"] : "9788204112170";
 
 $bibsys = new Bibsys();
-
+$bokkilden = new Bokkilden();
 $db = new DB();
 $regnSession = new RegnSession($db);
 $regnSession->auth();
@@ -17,6 +18,10 @@ $regnSession->auth();
 switch ($action) {
 	case "bibsys" :
 		$bookInfo = $bibsys->getBookInfo($isbn);
+		echo json_encode($bookInfo);
+		break;
+	case "bokkilden";
+		$bookInfo = $bokkilden->getBookInfo($isbn);
 		echo json_encode($bookInfo);
 		break;
 }
